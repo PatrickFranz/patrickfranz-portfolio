a42e31cfaf316cd15b57560a64be0adfacb23ed1
Patrick Franz <clymbing@gmail.com>
2021-04-07 11:31:34 -0700

    FIX: Adjust text size for smaller screens

--- gatsby/src/components/Footer.js
+++ gatsby/src/components/Footer.js
@@ -7,6 +7,12 @@ const StyledFooter = styled.footer`
   p {
     text-align: center;
   }
+  @media (max-width: 1200px) {
+    font-size: 3rem;
+  }
+  @media (max-width: 600px) {
+    font-size: 1.5rem;
+  }
 `;
 
 export default function Footer() {
